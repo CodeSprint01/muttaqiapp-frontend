1e4e4a1b877f6b25ce030bf5d9088a7b3c2503a8
CodeSprint <codesprintsite@gmail.com>
2024-01-25 15:44:29 +0500

    Changes screen from traker to add to do screens

--- src/screens/Tracker/TrackerScreen.tsx
+++ src/screens/Tracker/TrackerScreen.tsx
@@ -1,36 +1,14 @@
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import AppContainer from '../../components/atoms/app-container/AppContainer';
-import ScreenHeader from '../../components/molecules/app-header/ScreenHeader';
-import UserProgressCard from './UserProgressCard';
-import TodoTask from './TodoTask';
 
 const TrackerScreen = () => {
   return (
-    <AppContainer>
-      <ScrollView>
-        <View style={{paddingHorizontal: 20}}>
-          <ScreenHeader headerText="Planner" />
-        </View>
-        <View style={styles.container}>
-          <UserProgressCard totalTask={8} completeTask={6} circleValue={60} />
-          <View style={styles.todoConntainer}>
-            <TodoTask handleAddTodo={() => console.log('add to do')} />
-          </View>
-        </View>
-      </ScrollView>
-    </AppContainer>
+    <View>
+      <Text>TrackerScreen</Text>
+    </View>
   );
 };
 
 export default TrackerScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    flex: 1,
-  },
-  todoConntainer: {
-    paddingHorizontal: 20,
-  },
-});
+const styles = StyleSheet.create({});
--- src/screens/add-todo-screen/AddTodoScreen.tsx
+++ src/screens/add-todo-screen/AddTodoScreen.tsx
@@ -1,15 +1,36 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import React from 'react';
 import AppContainer from '../../components/atoms/app-container/AppContainer';
+import ScreenHeader from '../../components/molecules/app-header/ScreenHeader';
+import UserProgressCard from './UserProgressCard';
+import TodoTask from './TodoTask';
 
 const AddTodoScreen = () => {
   return (
-    <AppContainer style={{backgroundColor: 'red', flex: 1}}>
-      <Text>AddTodoScreen</Text>
+    <AppContainer>
+      <ScrollView>
+        <View style={{paddingHorizontal: 20}}>
+          <ScreenHeader headerText="Planner" />
+        </View>
+        <View style={styles.container}>
+          <UserProgressCard totalTask={8} completeTask={6} circleValue={60} />
+          <View style={styles.todoConntainer}>
+            <TodoTask handleAddTodo={() => console.log('add to do')} />
+          </View>
+        </View>
+      </ScrollView>
     </AppContainer>
   );
 };
 
 export default AddTodoScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    flex: 1,
+  },
+  todoConntainer: {
+    paddingHorizontal: 20,
+  },
+});
